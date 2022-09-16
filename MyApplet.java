c0345ddc0384ec0102662701c93a73cb3b589f8e
manishhamal2030 <87168495+manishhamal2030@users.noreply.github.com>
2022-09-16 06:45:54 +0545

    Add files via upload

---
+++ MyApplet.java
@@ -0,0 +1,18 @@
+import java.applet.*;
+import java.awt.*;
+
+public class MyApplet extends Applet{
+
+    public void paint(Graphics g)
+    {
+        g.drawLine (20,100,200,100);
+        g.drawLine (20,100,20,200);
+        
+        
+    }
+    
+}
+//<applet code = "MyApplet.class" width= 100 height=100></applet>
+
+    
+
